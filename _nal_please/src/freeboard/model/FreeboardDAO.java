abf838ac55b6e354d59f6496ad45b3a260005222
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-12-19 18:08:23 +0900

    DAO 대강

--- _nal_please/src/freeboard/model/FreeboardDAO.java
+++ _nal_please/src/freeboard/model/FreeboardDAO.java
@@ -1,5 +1,7 @@
 package freeboard.model;
 
+import java.util.ArrayList;
+
 public class FreeboardDAO {
 	
 	private static FreeboardDAO instance;
@@ -10,15 +12,50 @@ public class FreeboardDAO {
 	}
 	
 	private static FreeboardDAO getInstance() {
-		
 		if(instance == null) {
 			instance = new FreeboardDAO();
 		}
-		
 		return instance;
+	}
+	
+	public int getFreeboardListCount() {
+		int freeboardlistcnt = 0;
+		
+		return freeboardlistcnt;
+	}
+	
+	public ArrayList<FreeboardVO> getFreeboardList() {
+		ArrayList<FreeboardVO> volist = null;
+		
+		return volist;
+	}
+	
+	public void updateHit(int articleno) {
 		
 	}
 	
+	public FreeboardVO getArticleByArticleno(int articleno, int page) {
+		FreeboardVO vo = new FreeboardVO();
+		
+		return vo;
+	}
+
+	public String getLoginNameById(String id) {
+		String loginname = null;
+		
+		return loginname;
+	}
 	
+	public void insertArticle(FreeboardVO vo) {
+		
+	}
+		
+	public void updateArticle(FreeboardVO vo) {
+		
+	}
+	
+	public void deleteArticle(int articleno) {
+		
+	}
 	
 }
