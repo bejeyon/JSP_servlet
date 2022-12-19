f50e2ee9ec2be7d99dd89bb41737c9a7053a6a52
bejeyon <115999810+bejeyon@users.noreply.github.com>
2022-12-19 16:55:00 +0900

    20221219_DAO메서드만들기직전

--- _nal_please/src/freeboard/model/FreeboardDAO.java
+++ _nal_please/src/freeboard/model/FreeboardDAO.java
@@ -1,5 +1,24 @@
 package freeboard.model;
 
 public class FreeboardDAO {
+	
+	private static FreeboardDAO instance;
 
+	private FreeboardDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static FreeboardDAO getInstance() {
+		
+		if(instance == null) {
+			instance = new FreeboardDAO();
+		}
+		
+		return instance;
+		
+	}
+	
+	
+	
 }
--- _nal_please/src/freeboard/model/FreeboardVO.java
+++ _nal_please/src/freeboard/model/FreeboardVO.java
@@ -1,5 +1,82 @@
 package freeboard.model;
 
 public class FreeboardVO {
+	private int articleno;
+	private String title;
+	private String content;
+	private String writedate;
+	private String member_id;
+	private String member_name;
+	private int hit;
+	private int deletion;
 
+	public FreeboardVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getArticleno() {
+		return articleno;
+	}
+
+	public void setArticleno(int articleno) {
+		this.articleno = articleno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWritedate() {
+		return writedate;
+	}
+
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getMember_name() {
+		return member_name;
+	}
+
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public int getDeletion() {
+		return deletion;
+	}
+
+	public void setDeletion(int deletion) {
+		this.deletion = deletion;
+	}
+	
 }
